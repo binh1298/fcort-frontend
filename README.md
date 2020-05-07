430993f1485c0812fe7f8601e46b63319cc9ad77
Binh Pham <binh1298@gmail.com>
2020-05-07 09:04:00 +0700

    Init minimal webpack react app

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-This is an FCode's Members' Project
+# This is an FCode's Members' Project
+
+## Team Members
+
+Phạm Đức Bình
+Nguyễn Trần Thiên Đức
+Trần Bảo
---
+++ src/containers/App/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return <h1>Hello FCort</h1>;
+  }
+}
+
+export default App;
