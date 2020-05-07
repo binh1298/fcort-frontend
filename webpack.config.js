713bf2c3b434ff79a8eabba0a7b5b8c28986b00b
Binh Pham <binh1298@gmail.com>
2020-05-07 10:29:23 +0700

    Fix prettierrc: Use single quotes

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
-const webpack = require("webpack");
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
-  mode: "development",
-  entry: path.resolve(__dirname, "src", "index.js"),
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
-    path: path.resolve(__dirname, "build/"),
-    filename: "bundle.js",
-    publicPath: "/",
+    path: path.resolve(__dirname, 'build/'),
+    filename: 'bundle.js',
+    publicPath: '/',
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: "babel-loader",
+        use: 'babel-loader',
         exclude: /node_modules/,
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
   ],
   devServer: {
-    contentBase: "./build",
+    contentBase: './build',
     port: 3000,
     historyApiFallback: true,
   },
