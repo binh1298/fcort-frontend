385df6108f06a8e1d9de12ffb04fe6b7f9452c5d
thienduc12311 <59289834+thienduc12311@users.noreply.github.com>
2020-05-09 20:03:00 +0700

    add css to login form

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,21 +1,22 @@
-import React, {useContext} from 'react';
+/* eslint-disable prettier/prettier */
+import React, { useContext } from 'react';
 import ThemeContext from '../../contexts/ThemeContext';
 import Login from '../Login';
 import './styles.css';
 
 export const App = () => {
-  const theme = useContext(ThemeContext);
-  const styles = {
-    color: theme.palette.primary.main,
-  };
-  return (
-    <div>
-      <h1 className="example-header" style={styles}>
+	const theme = useContext(ThemeContext);
+	const styles = {
+		color: theme.palette.primary.main,
+	};
+	return (
+		<div>
+			{/* <h1 className="example-header" style={styles}>
         Hello FCort
-      </h1>
-      <Login />
-    </div>
-  );
+      </h1> */}
+			<Login />
+		</div>
+	);
 };
 
 export default App;
--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,42 +1,46 @@
-import React, {Component} from 'react';
+/* eslint-disable prettier/prettier */
+import React, { Component } from 'react';
+import imgUrl from './imgUrl';
+import './style.css';
 class Login extends Component {
-  render() {
-    return (
-      <body>
-        <img class="wave" src="img/wave.png" />
-        <div class="container">
-          <div class="img">
-            <img src="img/logo.png" />
-          </div>
-          <div class="login-content">
-            <form action="index.html">
-              <img src="img/logo.png" />
-              <h2 class="title">Welcome</h2>
-              <div class="input-div one">
-                <div class="i">
-                  <i class="fas fa-user"></i>
-                </div>
-                <div class="div">
-                  <h5>Username</h5>
-                  <input type="text" class="input" />
-                </div>
-              </div>
-              <div class="input-div pass">
-                <div class="i">
-                  <i class="fas fa-lock"></i>
-                </div>
-                <div class="div">
-                  <h5>Password</h5>
-                  <input type="password" class="input" />
-                </div>
-              </div>
-              <a href="#">Forgot Password?</a>
-              <input type="submit" class="btn" value="Login" />
-            </form>
-          </div>
-        </div>
-      </body>
-    );
-  }
+
+	render() {
+		return (
+			<body>
+				<img className="wave" src={imgUrl.background} />
+				<div className="container">
+					<div className="img">
+						<img src={imgUrl.logo} />
+					</div>
+					<div className="login-content">
+						<form action="index.html">
+							<img src={imgUrl.logo} />
+							<h2 className="title">Welcome</h2>
+							<div className="input-div one">
+								<div className="i">
+									<i className="fas fa-user"></i>
+								</div>
+								<div className="div">
+									<h5>Username</h5>
+									<input type="text" className="input" />
+								</div>
+							</div>
+							<div className="input-div pass">
+								<div className="i">
+									<i className="fas fa-lock"></i>
+								</div>
+								<div className="div">
+									<h5>Password</h5>
+									<input type="password" className="input" />
+								</div>
+							</div>
+							<a href="#">Forgot Password?</a>
+							<input type="submit" className="btn" value="Login" />
+						</form>
+					</div>
+				</div>
+			</body>
+		);
+	}
 }
 export default Login;
