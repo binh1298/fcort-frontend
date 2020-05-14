192607ccac30a171ca36e3ce93b9b1dcad0e0e8c
Trung Kien <kien123456k@gmail.com>
2020-05-14 18:41:22 +0700

    create Home component

---
+++ src/containers/Home/index.js
@@ -0,0 +1,8 @@
+import React, {useContext} from 'react';
+import ThemeContext from '../../contexts/ThemeContext';
+import avatar from '../../assets/images/avatar.jpg';
+
+export const Home = () => {
+  const theme = useContext(ThemeContext);
+};
+export default Home;
