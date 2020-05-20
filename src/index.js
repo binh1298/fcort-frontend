1ee53e2d4610d7bde69cec7153fbe993e7d744b4
Binh Pham <binh1298@gmail.com>
2020-05-20 20:47:11 +0700

    Implement regerator runtime

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'regenerator-runtime/runtime.js';
 import App from './containers/App';
 import './styles.scss';
 import 'font-awesome/css/font-awesome.css';
