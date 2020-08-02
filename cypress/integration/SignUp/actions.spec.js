c79c8913cd086b56136acbf9d31fa652348a16e2
thienduc12310 <thienduc12310@gmail.com>
2020-08-02 23:51:42 +0700

    Login and Signup test

--- cypress/integration/SignUp/actions.spec.js
+++ cypress/integration/SignUp/actions.spec.js
@@ -3,12 +3,15 @@
 context('Actions', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/signup');
-    cy.get('.input-container > div> input').eq(0).type('fake17@email.com');
+    cy.get('.input-container > div> input').eq(0).type('fake19@email.com');
     cy.get('.input-container > div > input').eq(1).type('ducdeptrai');
     cy.get('.input-container > div > input').eq(2).type('ducdeptrai');
     cy.get('form').submit();
   });
-
+  afterEach(() => {
+    cy.get('.input-container > div > input').eq(0).type('Thien Duc');
+    cy.get('form').submit();
+  });
   it('check the url', () => {
     cy.url().should('eq', 'http://localhost:3000/');
   });
